ceb4db95ee061812c12e99b7678ed9e88aa31eb9
atef <devops@gmail.com>
2026-09-03 12:03:01 +0300

    feat: add product catalog and home page components, implement image optimization, and enhance footer and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,22 @@
 import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
+import { Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage";
+
 
 function App() {
-  const { isLoaded } = useAuth();
+  const { isLoaded, isSignedIn } = useAuth();
 
   if (!isLoaded) return <PageLoader />;
 
   return (
     <Layout>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-
-      <p className="text-red-500 font-extrabold text-4xl bg-blue">Hello</p>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-secondary">Click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        
+       
+      </Routes>
     </Layout>
   );
 }
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,13 +6,10 @@ function Layout({ children }) {
     <div className="flex min-h-svh flex-col bg-base-200 text-base-content">
       <Navbar />
 
-      <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-8 md:px-6 md:py-10">
-        {children}
-      </main>
+      <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-8 md:px-6 md:py-10">{children}</main>
 
       <Footer />
     </div>
   );
 }
-
 export default Layout;
